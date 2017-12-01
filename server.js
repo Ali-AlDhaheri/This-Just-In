5cc54b7234b66f1b1836e99c9992eac37b2fd583
Ali-AlDhaheri <31647294+Ali-AlDhaheri@users.noreply.github.com>
2017-12-01 17:10:03 +0400

    Update with better json

--- server.js
+++ server.js
@@ -1,5 +1,3 @@
-
-
 var express = require('express');
 var cheerio = require('cheerio');
 var request = require('request');
@@ -13,18 +11,26 @@ request(url, function(err, resp, body) {
   var $ = cheerio.load(body);
 
   var title = $('.featured-title');
+  
+  var til = [];
 
   for (var i = 0; i < title.length; i++){
   	console.log(title[i].children[3].children[0].children[0].data);
-  }
-  var more = $('.cat-box-content')
-  for (var i = 0; i < more.length; i++){
-  	console.log(more[i]);
-  }
-  titleText = title.text().replace(/\t/g, '');
 
-  var titleString = JSON.stringify(titleText);
-  fs.writeFile("sana.json", titleString);
+    til.push(title[i].children[3].children[0].children[0].data);
+
+
+    var titleString = JSON.stringify(til);
+    fs.writeFile("sana.json", titleString, function(err) {
+    if (err) throw err;
+    console.log('complete');
+    });
+  }
+  
+  var jsonContent = require('./sana.json');
+  var r1 = sentiment(jsonContent);
+  console.log(r1);
+
 
 });
 
